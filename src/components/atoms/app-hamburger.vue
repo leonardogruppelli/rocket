85812cf9ac8e1ad06598d0e829e4e334dd6d08b6
Leonardo Gruppelli <leonardo.gruppelli@melhorenvio.com>
2022-10-27 12:27:48 -0300

    feat: add app-hamburger

---
+++ src/components/atoms/app-hamburger.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useUiStore } from '@/store/ui';
+
+const uiStore = useUiStore();
+
+const { openMenu } = uiStore;
+</script>
+
+<template>
+  <button
+    class="w-8 h-8 flex flex-col items-center justify-center gap-1 px-1 cursor-pointer lg:hidden"
+    @click.stop="openMenu"
+  >
+    <span
+      v-for="line in 3"
+      :key="line"
+      class="w-full h-[2px] bg-white rounded-sm"
+    ></span>
+  </button>
+</template>
