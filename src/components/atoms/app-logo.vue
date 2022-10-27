007269e3c8a86162c34c594e5638728c8447f517
Leonardo Gruppelli <leonardo.gruppelli@melhorenvio.com>
2022-10-27 13:04:35 -0300

    feat: add negative prop to app-logo

--- src/components/atoms/app-logo.vue
+++ src/components/atoms/app-logo.vue
@@ -1,7 +1,35 @@
+<script setup>
+defineProps({
+  negative: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
 <template>
-  <a href="#model" class="flex text-2xl font-bold uppercase">
+  <a
+    href="#model"
+    class="flex text-2xl font-bold uppercase"
+  >
     <p>Ro</p>
-    <p class="text-yellow-500">.</p>
-    <p class="text-red-500">cket</p>
+
+    <p
+      class="text-yellow-500"
+      :class="{
+        'text-gray-800': negative,
+      }"
+    >
+      .
+    </p>
+
+    <p
+      class="text-red-500"
+      :class="{
+        'text-gray-800': negative,
+      }"
+    >
+      cket
+    </p>
   </a>
 </template>
