44e39f229c69c6be16b6cc7266b8f4b09b3a3c47
Leonardo Gruppelli <leonardo.gruppelli@melhorenvio.com>
2022-10-27 11:47:11 -0300

    feat: add pinia store

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue';
 import { TroisJSVuePlugin } from 'troisjs';
+import { createPinia } from 'pinia';
 import App from './app.vue';
 
 import './styles/app.postcss';
 
+const pinia = createPinia();
 const app = createApp(App);
 
+app.use(pinia);
 app.use(TroisJSVuePlugin);
 
 app.mount('#app');
